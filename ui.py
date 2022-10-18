d410edeb8c1082eb7b56cfb49a6026c7d1d60c37
Chakib Rached, Pharm.D <chakibrachedi@pm.me>
2022-10-18 13:30:42 +0100

    init commit

---
+++ ui.py
@@ -0,0 +1,54 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125, width=200, text="Some Text", font=("Arial", 15, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="images/true.png")
+        self.true_btn = Button(image=true_img, highlightthickness=0, command=self.true_press)
+        self.true_btn.grid(column=0, row=2)
+
+        false_img = PhotoImage(file="images/false.png")
+        self.false_btn = Button(image=false_img, highlightthickness=0, command=self.false_press)
+        self.false_btn.grid(column=1, row=2)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.true_btn.config(state="disabled")
+            self.false_btn.config(state="disabled")
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the Quiz!")
+    def true_press(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+    def false_press(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
